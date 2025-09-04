f23ab474be19221854ddff678db509b3de10f118
Shreya Das <shreyadas0429@gmail.com>
2025-09-05 00:04:39 +0600

    Add files via upload

---
+++ forreallife4.java
@@ -0,0 +1,8 @@
+public class forreallife4 {
+  public static void main(String[] args) {
+    for (int seat = 1; seat <= 5; seat++) {
+      System.out.println("Seat number: " + seat);
+    }
+  }
+}
+
