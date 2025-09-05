1eef78370397ca72fa2274a2606e0ab524fd635b
Shreya Das <shreyadas0429@gmail.com>
2025-09-05 10:42:22 +0600

    Add files via upload

---
+++ array12.java
@@ -0,0 +1,7 @@
+public class array12 {
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+    System.out.println(myNumbers[0][1]);
+  }
+}
+
---
+++ array16.java
@@ -0,0 +1,12 @@
+public class array16 {
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+    for (int[] row : myNumbers) {
+      for (int num : row) {
+        System.out.println(num);
+      }
+    }
+  }
+}
+
